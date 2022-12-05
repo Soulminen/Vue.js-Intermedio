1aac53b18956986097bc054dc04e51f8085807bc
Yamil Escobar <joseyamilej@hotmail.com>
2022-12-05 23:20:59 +0100

    Realizar peticion HTTP basica

---
+++ src/characters/components/CardList.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import breakingBadApi from '@/api/breakingBadApi';
+
+breakingBadApi.get('/characters')
+    .then( resp => {
+        console.log({ data: resp.data[0].name });
+    });
+
+</script>
+
+
+<template>
+    <ul>
+        <li>Hola</li>
+        <li>Hola</li>
+        <li>Hola</li>
+    </ul>
+</template>
+
+
+<style scoped>
+
+</style>
