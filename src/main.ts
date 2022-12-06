8fbd475c7a1f47d6bc067088d6266d118c827d34
Yamil Escobar <joseyamilej@hotmail.com>
2022-12-06 22:44:02 +0100

    Vue TanStack Query

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 
 
@@ -8,6 +9,9 @@ import './assets/main.css';
 
 const app = createApp(App);
 
-app.use(router);
+
+app.use( VueQueryPlugin )
+
+app.use( router );
 
 app.mount('#app');
