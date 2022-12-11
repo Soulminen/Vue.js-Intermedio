f566dc104bb0402d734e691368d0d26c4ec6c40f
Yamil Escobar <joseyamilej@hotmail.com>
2022-12-11 17:38:28 +0100

    Modificando CharacterList con lo que teniamos en CardList

--- src/characters/components/CardList.vue
+++ src/characters/components/CardList.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
 import CharacterCard from "@/characters/components/CharacterCard.vue";
+import type { Character } from "../interfaces/character";
 
-import breakingBadApi from '@/api/breakingBadApi';
-import type { Character } from '@/characters/interfaces/character';
-import { useCharacters } from '@/characters/composables/useCharacters';
-import { useQuery } from "@tanstack/vue-query";
+
+interface Props {
+    characters: Character[],
+}
+
+const props = defineProps<Props>();
+
+
+
+// import { ref } from "vue";
+// import breakingBadApi from '@/api/breakingBadApi';
+// import type { Character } from '@/characters/interfaces/character';
+// import { useCharacters } from '@/characters/composables/useCharacters';
+// import { useQuery } from "@tanstack/vue-query";
 
 //! 1- Normal suspense
 // const { data: characters } = await breakingBadApi.get<Character[]>('/characters');
@@ -15,36 +25,36 @@ import { useQuery } from "@tanstack/vue-query";
 // const { isLoading, characters, hasError, errorMessage } = useCharacters();
 
 //! 3- TanStack.Query
-const getCharactersSlow = async():Promise<Character[]> => {
+// const getCharactersSlow = async():Promise<Character[]> => {
 
-    return new Promise( (resolve) => {
-        setTimeout( async() => {
-            const { data } = await breakingBadApi.get<Character[]>('/characters');
-            resolve(data.filter( character => ![14, 17, 39].includes(character.char_id) ) );
-        }, 1000);
-    })
+//     return new Promise( (resolve) => {
+//         setTimeout( async() => {
+//             const { data } = await breakingBadApi.get<Character[]>('/characters');
+//             resolve(data.filter( character => ![14, 17, 39].includes(character.char_id) ) );
+//         }, 1);
+//     })
 
-}
-const { isLoading, isError, data: characters, error} = useQuery(
-    ['characters'],
-    getCharactersSlow,
-    {
-        cacheTime: 1000 * 60,
-        refetchOnReconnect: 'always'
+// }
+// const { isLoading, isError, data: characters, error} = useQuery(
+//     ['characters'],
+//     getCharactersSlow,
+//     // {
+//     //     cacheTime: 1000 * 60,
+//     //     refetchOnReconnect: 'always'
         
-    }
-);
+//     // }
+// );
 
 
 </script>
 
 
 <template>
-    <h1 v-if="isLoading">Loading...</h1>
+    <!-- <h1 v-if="isLoading">Loading...</h1> -->
     
     <div class="card-list">
         <CharacterCard   
-            v-for="character of characters"
+            v-for="character of props.characters"
             :key="character.char_id" 
             :character="character"
         />
--- src/characters/pages/CharacterList.vue
+++ src/characters/pages/CharacterList.vue
@@ -1,20 +1,42 @@
 <script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query';
 
+import breakingBadApi from '@/api/breakingBadApi';
 import CardList from '@/characters/components/CardList.vue';
 
+import type { Character } from '@/characters/interfaces/character';
+
 const props = defineProps<{ title: string, visible: boolean}>();
+
+const getCharacters = async(): Promise<Character[]> => {
+        const { data } = await breakingBadApi.get<Character[]>('/characters');
+        return data.filter( character => ![14, 17, 39].includes(character.char_id) )
+}
+
+
+const { isLoading, data: characters } = useQuery(
+        ['characters'],
+        getCharacters
+
+);
+
 
 </script>
 
 
 <template>
-    
+
+     <h1 v-if="isLoading">Loading...</h1>
+
+     <template v-else>
+     
         <h2>{{ props.title}}</h2>
 
-        <CardList />
+        <CardList :characters="characters || []" />
+
+     </template>
 
 
-    
 </template>
 
 
