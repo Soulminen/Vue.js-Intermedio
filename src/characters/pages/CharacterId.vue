1a4f469f74da7486c7bcd0aa579667a369638160
Yamil Escobar <joseyamilej@hotmail.com>
2022-12-27 22:42:01 +0100

    Cargar información del personaje

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -1,11 +1,38 @@
 
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { useQuery } from '@tanstack/vue-query';
+
+import breakingBadApi from '@/api/breakingBadApi';
+import characterStore from '@/store/characters.store';
+import type { Character } from '@/characters/interfaces/character';
 
 
 const route = useRoute();
 
 const { id } = route.params as { id: string };
+
+
+const getCharacterCacheFirst = async( characterId: string ):Promise<Character> => {
+
+    if ( characterStore.checkIdInStore(characterId) ) {
+        return characterStore.ids.list[characterId];
+    }
+
+    const { data } = await breakingBadApi.get<Character[]>(`/characters/${ characterId }`);
+    return data[0];
+}
+
+const { data } = useQuery(
+    [ 'characters', id],
+    () => getCharacterCacheFirst(id),
+    {
+        onSuccess( character ) {
+            characterStore.loadedCharacter(character);
+        }
+    }
+)
+
 
 </script>
 
@@ -13,6 +40,9 @@ const { id } = route.params as { id: string };
 <template>
     <div>
         <h1>Character #{{ id }}</h1>
+        <code>
+        {{ data }}
+        </code>
     </div>
 </template>
 
