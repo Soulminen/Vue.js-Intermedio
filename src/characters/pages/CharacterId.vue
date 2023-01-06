df450f067e2b06f593adf76755959c797b1322d0
Yamil Escobar <joseyamilej@hotmail.com>
2023-01-06 15:47:48 +0100

    Manejo de los errores en el composable

--- src/characters/pages/CharacterId.vue
+++ src/characters/pages/CharacterId.vue
@@ -2,20 +2,19 @@
 import { useRoute } from 'vue-router';
 import useCharacter from '../composables/useCharacter';
 
-
 const route = useRoute();
 
 const { id } = route.params as { id: string };
 
-const { character, hasError, errorMessage } = useCharacter( id );
+const { character, hasError, errorMessage, isLoading } = useCharacter( id );
 
 </script>
 
 <template>
-    <h1 v-if="!character">Loading...</h1>
+    <h1 v-if="isLoading">Loading...</h1>
     <h1 v-else-if="hasError">{{ errorMessage }}</h1>
 
-    <div v-else>
+    <div v-else-if="character">
         <h1>{{ character.name }}</h1>
         <div class="character-container">
             <img src="character.img" alt="character.name">
